a848d7bc9160f1c1da151d8bd1df404456cedb74
Abraham-Thomas <15197498389@163.com>
2019-12-21 00:31:25 +0800

    import element

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 引入elementui
+import ElementUI from 'element-ui'
+// 单独引入样式文件
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 执行elementui，后面可以传入一个配置对象
+Vue.use(ElementUI, { size: 'small', zIndex: 3000 })
+
 Vue.config.productionTip = false
 
 new Vue({
